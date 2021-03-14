b47baff3419a710ca56628a88477ccf3c58f2fb0
Diyar Ceran <yakupceran00@gmail.com>
2021-03-14 12:37:43 +0100

    yeni güncelleme
    bütçe uygulaması tasarım kısımları güncellemesi

--- normal çaplı bütçe uygulaması/src/app/app.module.ts
+++ normal çaplı bütçe uygulaması/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './component/main-page/main-page.component';
 import { AddItemFormComponent } from './component/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './component/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './component/budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
